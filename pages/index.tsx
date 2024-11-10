258b377f68f3bd3d6d83f3098e4452827de59b05
hyfetch <128172119+hyfetch@users.noreply.github.com>
2024-11-10 11:10:48 +0100

    Slight Re-write

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,37 +1,50 @@
 import React, { useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTelegram, faDiscord, faSignalMessenger } from '@fortawesome/free-brands-svg-icons'; // Import the signal-messenger icon
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons'; // Correct import for faEnvelope
-
+import { faTelegram, faDiscord, faSignalMessenger, faKeybase } from '@fortawesome/free-brands-svg-icons'; // Import the signal-messenger icon
 const Home: React.FC = () => {
   useEffect(() => {
-    document.title = "Contact me.";
+
+    document.title = "protogen mail";
+
+    // Dynamically set the favicon
+    const setFavicon = () => {
+      const link = document.createElement('link');
+      link.rel = 'icon';
+      link.href = '/images/peek.png'; 
+      document.head.appendChild(link);
+    };
+
+    setFavicon(); 
   }, []);
+
 
   return (
     <div className="container">
       {/* Text Container */}
       <div className="text-container">
-          <img id="name" src="/images/peek.png" alt="Peek" /> {/* Display the image */}
-      </div>
+          <img id="name" src="/images/peek.png" alt="Peek" style={{width: "50px", height: "50px"}} />
+          </div>
+
 
       {/* Icons Container */}
       <div className="icons-container">
         <a href="https://t.me/uwufetch" target="_blank" rel="noopener noreferrer" className="icon">
           <FontAwesomeIcon icon={faTelegram} size="2x" />
         </a>
-        <a href="https://signal.me/#eu/E_mXZC3kXOSBBcyNjq9JrVlrx11S4wTtauFWxyOa4o4ZKjeVIsxI47opy_LmaOpu" target="_blank" rel="noopener noreferrer" className="icon">
+        <a href="https://url.mero.lol/signal" target="_blank" rel="noopener noreferrer" className="icon">
           <FontAwesomeIcon icon={faSignalMessenger} size="2x" />
         </a>
-        <a href="https://dc.mero.lol" target="_blank" rel="noopener noreferrer" className="icon">
+        <a href="https://url.mero.lol/discord" target="_blank" rel="noopener noreferrer" className="icon">
           <FontAwesomeIcon icon={faDiscord} size="2x" />
         </a>
-        <a href="mailto:contact@mero.lol" className="icon">
-          <FontAwesomeIcon icon={faEnvelope} size="2x" />
+        <a href="https://keybase.io/hyfetch" className="icon">
+          <FontAwesomeIcon icon={faKeybase} size="2x" />
         </a>
       </div>
       <p className="footer">
-        Based on <a href="https://nwunder.com" target="_blank" rel="noopener noreferrer">nwunder's</a> website design.
+      <a href="https://nwunder.com">
+    <img src="https://nwunder.com/assets/bongo.gif" alt="NWunder Credit" title="Based on nwunder's site" width="64" height="64" />
+</a>
       </p>
     </div>
   );
